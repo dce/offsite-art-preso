4e0e7a289e605bb72ede00c4c3214c16cc5f9f83
David Eisinger <david.eisinger@gmail.com>
2022-10-14 14:25:03 -0400

    Randomization/pixelization

--- dce/main.go
+++ dce/main.go
@@ -4,18 +4,33 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"math/rand"
 	"os"
+	"time"
 )
 
 const WIDTH = 800
 const HEIGHT = 600
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	srcImg := image.NewRGBA(image.Rect(0, 0, WIDTH/2, HEIGHT/2))
+
+	for x := 0; x < WIDTH/2; x++ {
+		for y := 0; y < HEIGHT/2; y++ {
+			setPixel(srcImg, x, y)
+		}
+	}
+
 	img := image.NewRGBA(image.Rect(0, 0, WIDTH, HEIGHT))
 
-	for x := 0; x < WIDTH; x++ {
-		for y := 0; y < HEIGHT; y++ {
-			setPixel(img, x, y)
+	for x := 0; x < WIDTH/2; x++ {
+		for y := 0; y < HEIGHT/2; y++ {
+			img.Set(2*x, 2*y, srcImg.At(x, y))
+			img.Set(2*x+1, 2*y, srcImg.At(x, y))
+			img.Set(2*x, 2*y+1, srcImg.At(x, y))
+			img.Set(2*x+1, 2*y+1, srcImg.At(x, y))
 		}
 	}
 
@@ -25,5 +40,15 @@ func main() {
 }
 
 func setPixel(img *image.RGBA, x int, y int) {
-	img.Set(x, y, color.RGBA{R: 255, G: 255, B: 255, A: 255})
+	LBLUE := color.RGBA{R: 68, G: 146, B: 179, A: 255}
+	DBLUE := color.RGBA{R: 21, G: 45, B: 62, A: 255}
+	ORANGE := color.RGBA{R: 222, G: 114, B: 56, A: 255}
+
+	colors := [3]color.RGBA{ORANGE, LBLUE, DBLUE}
+
+	col := colors[rand.Intn(3)]
+
+	// lighten := uint8(255 * (float64(x) / float64(WIDTH)))
+
+	img.Set(x, y, col)
 }
